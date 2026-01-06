3c1491bb489a1008b71021456049fff737a12d9c
prasanna-testpress <prasanna@testpress.in>
2026-01-06 14:43:33 +0530

    Register core models in Django admin
    Configure Django admin for core domain models to enable easy
    management of restaurants, menus, reviews, and user interactions
    during development and testing.

--- apps/restaurants/admin.py
+++ apps/restaurants/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Cuisine,
+    Restaurant,
+    MenuItem,
+    RestaurantImage,
+)
+
+
+@admin.register(Cuisine)
+class CuisineAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "city",
+        "veg_type",
+        "cost_for_two",
+        "is_open",
+        "is_spotlight",
+    ]
+    list_filter = ["city", "veg_type", "is_open", "is_spotlight"]
+    search_fields = ["name", "city"]
+    filter_horizontal = ["cuisines"]
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ["name", "restaurant", "price"]
+    list_filter = ["restaurant"]
+    search_fields = ["name", "restaurant__name"]
+
+
+@admin.register(RestaurantImage)
+class RestaurantImageAdmin(admin.ModelAdmin):
+    list_display = ["restaurant", "uploaded_at"]
+    list_filter = ["restaurant"]
