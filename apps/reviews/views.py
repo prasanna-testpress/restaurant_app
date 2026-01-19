aa011d18a512c8d837c8643143b36c32aac63706
prasanna-testpress <prasanna@testpress.in>
2026-01-19 14:27:51 +0530

    add genric method

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,7 +1,7 @@
+from django.urls import reverse
+from django.views.generic import FormView, DeleteView
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
-from django.views import View
 from apps.reviews.forms import ReviewForm
 from apps.reviews.models import Review
 from apps.restaurants.models import Restaurant
@@ -10,21 +10,24 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class ReviewSubmitView(LoginRequiredMixin, View):
-    def post(self, request, restaurant_id, *args, **kwargs):
-        form = ReviewForm(request.POST)
+class ReviewSubmitView(LoginRequiredMixin, FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
 
-        if not form.is_valid():
-            return redirect("restaurants:restaurant_detail", id=restaurant_id)
-
+    def form_valid(self, form):
         self._submit_review(
-            user=request.user,
-            restaurant_id=restaurant_id,
+            user=self.request.user,
+            restaurant_id=self.kwargs["restaurant_id"],
             rating=form.cleaned_data["rating"],
             comment=form.cleaned_data["comment"],
         )
-        return redirect("restaurants:restaurant_detail", id=restaurant_id)
+        return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse(
+            "restaurants:restaurant_detail",
+            kwargs={"id": self.kwargs["restaurant_id"]},
+        )
         
     def _submit_review(self, user: User, restaurant_id: int, rating: int, comment: str) -> Review:
         restaurant = get_object_or_404(Restaurant, id=restaurant_id)
@@ -40,23 +43,16 @@ class ReviewSubmitView(LoginRequiredMixin, View):
         return review
 
 
-class ReviewDeleteView(LoginRequiredMixin, View):
-    def post(self, request, review_id, *args, **kwargs):
-        restaurant_id = self._delete_review(
-            user=request.user,
-            review_id=review_id,
+class ReviewDeleteView(LoginRequiredMixin, DeleteView):
+    model = Review
+    pk_url_kwarg = "review_id"
+
+    def get_queryset(self):
+        return Review.objects.filter(user=self.request.user)
+
+    def get_success_url(self):
+        return reverse(
+            "restaurants:restaurant_detail",
+            kwargs={"id": self.object.restaurant_id},
         )
-        return redirect("restaurants:restaurant_detail", id=restaurant_id)
-
-    def _delete_review(self, user: User, review_id: int) -> int:
-        review = get_object_or_404(
-                Review,
-                id=review_id,
-                user=user,
-            )
-
-        restaurant_id = review.restaurant_id
-        review.delete()
-
-        return restaurant_id
         
