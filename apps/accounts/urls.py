139430cf6aa0a63fb53d2d92a80fa3b5fa362bf0
prasanna-testpress <prasanna@testpress.in>
2026-01-20 14:28:06 +0530

    bug fix and add empty line

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -26,14 +26,14 @@ urlpatterns = [
         name="password_reset_done",
     ),
     path(
-        "reset/<uidb64>/<token>/",
+        "password-reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(
             template_name="accounts/password_reset_confirm.html",
         ),
         name="password_reset_confirm",
     ),
     path(
-        "reset/done/",
+        "password-reset/complete/",
         auth_views.PasswordResetCompleteView.as_view(
             template_name="accounts/password_reset_complete.html",
         ),
